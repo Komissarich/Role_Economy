34abe45fdd81bccf9090a7a9dce43b35bce4938c
komissarich <egorkart1@gmail.com>
2024-01-22 01:26:10 +0400

    done production

--- classes.js
+++ classes.js
@@ -102,18 +102,52 @@ class Household {
         }
     }
     //Example of spending_map
-    // const production_map = [
-	//new Map(["iron", 
-	//	new Map(["wheat", 2],
-	//		["ore", 3])
-	//       ]),
-    //	new Map(["equipment",
-	//	new Map(["wheat", 2],
-    //			["ore", 2])]) 
-    //]
+    // const spending_map = [
+    //    new Map([["iron", 
+     //         new Map([["wheat", 2],
+    //                  ["ore", 3]])
+    //          ]]),
+    //    new Map([["equipment",
+   //           new Map([["wheat", 2],
+   //                  ["ore", 2]])
+    //          ]])
+                  
+   //   ]
     //
     production() {
-        
+        for (good of this.production_map.keys()) {
+            for (i = 0; i < this.production_map.get(good); i++) {
+               //console.log("NOW MAKING", good)
+                is_productionable = true
+                let backup = new Map(storage)
+               // console.log("BACKUP", backup)
+                for (j = 0; j < this.spending_map.length; j++) {
+                    if (spending_map[j].get(good) != undefined) {
+                        for (resource_to_spend of this.spending_map[j].get(good).keys()) {
+                      //      console.log(resource_to_spend)
+                            if (storage.get(resource_to_spend) >= this.spending_map[j].get(good).get(resource_to_spend)) {
+                            //    console.log(storage)
+                                storage.set(resource_to_spend, this.storage.get(resource_to_spend) - this.spending_map[j].get(good).get(resource_to_spend))
+                           //     console.log(storage)
+                            }
+                            else {
+                                is_productionable = false
+                            }
+                        }
+                    }
+                }
+                if (is_productionable == true) {
+                  //  console.log("SUCCESFULLY", good)
+                }
+                else {
+                   // console.log("FAILED", good)
+                   // console.log(thisstorage)
+                  //  console.log("bb", backup)
+                    storage = backup
+                  //  console.log(storage)
+                }
+            }
+        }
     }
 
     consumption() {
--- test.js
+++ test.js
@@ -1,5 +1,61 @@
 function abc(b) {
-    console.log(`hello ${b}`)
+    production_map = new Map([
+        ["iron", 2],
+        ["equipment", 1]
+    ])
+
+    let storage = new Map([
+        ["wheat",15],
+        ["ore", 9]
+    ])
+
+
+    const spending_map = [
+          new Map([["iron", 
+        	    new Map([["wheat", 2],
+        		        ["ore", 3]])
+                ]]),
+          new Map([["equipment",
+        	    new Map([["wheat", 5],
+        			    ["ore", 6]])
+                ]])
+                    
+        ]
+
+    for (good of production_map.keys()) {
+        for (i = 0; i < production_map.get(good); i++) {
+           console.log("NOW MAKING", good)
+            is_productionable = true
+            let backup = new Map(storage)
+            console.log("BACKUP", backup)
+            for (j = 0; j < spending_map.length; j++) {
+                if (spending_map[j].get(good) != undefined) {
+                    for (resource_to_spend of spending_map[j].get(good).keys()) {
+                        console.log(resource_to_spend)
+                        if (storage.get(resource_to_spend) >= spending_map[j].get(good).get(resource_to_spend)) {
+                            console.log(storage)
+                            storage.set(resource_to_spend, storage.get(resource_to_spend) - spending_map[j].get(good).get(resource_to_spend))
+                            console.log(storage)
+                        }
+                        else {
+                            is_productionable = false
+                        }
+                    }
+                }
+            }
+            if (is_productionable == true) {
+                console.log("SUCCESFULLY", good)
+            }
+            else {
+                console.log("FAILED", good)
+                console.log(storage)
+                console.log("bb", backup)
+                storage = backup
+                console.log(storage)
+            }
+        }
+    }
+        
 }
 
 
